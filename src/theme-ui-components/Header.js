84b9833554a0568b0192d6be21454e876fa4c74f
Xavier Mirabelli-Montan <xavier.mm@investisdigital.com>
2021-11-30 17:46:54 +0000

    Added all header styles from theme-ui recipes.

--- src/theme-ui-components/Header.js
+++ src/theme-ui-components/Header.js
@@ -42,50 +42,152 @@ export const HeaderA1 = () => (
 );
 
 export const HeaderA2 = () => (
-    <header
-  sx={{
-    variant: 'styles.header',
-  }}>
-  <div
+  <header
     sx={{
+      variant: "styles.header",
+    }}
+  >
+    <div
+      sx={{
+        maxWidth: 768,
+        mx: "auto",
+        px: 3,
+        display: "flex",
+        alignItems: "baseline",
+      }}
+    >
+      <Link
+        to="/"
+        sx={{
+          variant: "styles.navlink",
+          fontSize: 5,
+          py: 2,
+        }}
+      >
+        Hello
+      </Link>
+      <div sx={{ mx: "auto" }} />
+      <Link
+        to="/blog"
+        sx={{
+          variant: "styles.navlink",
+          ml: 3,
+          py: 2,
+        }}
+      >
+        Blog
+      </Link>
+      <Link
+        to="/about"
+        sx={{
+          variant: "styles.navlink",
+          ml: 3,
+          py: 2,
+        }}
+      >
+        About
+      </Link>
+    </div>
+  </header>
+);
+
+export const HeaderA3 = () => (
+  <header
+    sx={{
+      display: "grid",
+      gridGap: 3,
       maxWidth: 768,
-      mx: 'auto',
+      mx: "auto",
       px: 3,
-      display: 'flex',
-      alignItems: 'baseline',
-    }}>
-    <Link
-      to="/"
+      py: 3,
+      gridAutoFlow: "row",
+      gridTemplateColumns: ["repeat(2, 1fr)", "repeat(3, 1fr)"],
+      variant: "styles.header",
+    }}
+  >
+    <div
       sx={{
-        variant: 'styles.navlink',
-        fontSize: 5,
-        py: 2,
-      }}>
-      Hello
-    </Link>
-    <div sx={{ mx: 'auto' }} />
-    <Link
-      to="/blog"
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gridColumnStart: [1, 2],
+        gridColumnEnd: [3, 3],
+        order: [0, 1],
+      }}
+    >
+      <Link to="/" title="Home">
+        <img
+          alt="UI Logo"
+          src="https://contrast.now.sh/cff/40f?size=48&fontSize=2&baseline=2&fontWeight=900&radius=32&text=UI"
+        />
+        <span
+          sx={{
+            position: "absolute",
+            width: 1,
+            height: 1,
+            overflow: "hidden",
+            top: -9999,
+          }}
+        >
+          Home
+        </span>
+      </Link>
+    </div>
+    <div
       sx={{
-        variant: 'styles.navlink',
-        ml: 3,
-        py: 2,
-      }}>
-      Blog
-    </Link>
-    <Link
-      to="/about"
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+      }}
+    >
+      <Link
+        to="/work"
+        sx={{
+          variant: "styles.navlink",
+          p: 2,
+        }}
+      >
+        Work
+      </Link>
+      <Link
+        to="/blog"
+        sx={{
+          variant: "styles.navlink",
+          p: 2,
+        }}
+      >
+        Blog
+      </Link>
+    </div>
+    <div
       sx={{
-        variant: 'styles.navlink',
-        ml: 3,
-        py: 2,
-      }}>
-      About
-    </Link>
-  </div>
-</header>
-  );
-  
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        order: 2,
+      }}
+    >
+      <Link
+        to="/about"
+        sx={{
+          variant: "styles.navlink",
+          p: 2,
+        }}
+      >
+        About
+      </Link>
+      <Link
+        to="/contact"
+        sx={{
+          variant: "styles.navlink",
+          p: 2,
+        }}
+      >
+        Contact
+      </Link>
+    </div>
+  </header>
+);
 
 export const HeaderA4 = () => (
   <header
@@ -183,4 +285,74 @@ export const HeaderA4 = () => (
       </Link>
     </div>
   </header>
+);
+
+export const HeaderA5 = () => (
+<header
+  sx={{
+    py: 4,
+    variant: 'styles.header',
+  }}>
+  <div
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      mb: 3,
+    }}>
+    <Link to="/" title="Home">
+      <img
+        alt="UI Logo"
+        src="https://contrast.now.sh/cff/40f?size=48&fontSize=2&baseline=2&fontWeight=900&radius=32&text=UI"
+      />
+      <span
+        sx={{
+          position: 'absolute',
+          width: 1,
+          height: 1,
+          overflow: 'hidden',
+          top: -9999,
+        }}>
+        Home
+      </span>
+    </Link>
+  </div>
+  <div
+    sx={{
+      display: 'flex',
+      justifyContent: 'center',
+    }}>
+    <Link
+      to="/work"
+      sx={{
+        variant: 'styles.navlink',
+        p: 2,
+      }}>
+      Work
+    </Link>
+    <Link
+      to="/blog"
+      sx={{
+        variant: 'styles.navlink',
+        p: 2,
+      }}>
+      Blog
+    </Link>
+    <Link
+      to="/about"
+      sx={{
+        variant: 'styles.navlink',
+        p: 2,
+      }}>
+      About
+    </Link>
+    <Link
+      to="/contact"
+      sx={{
+        variant: 'styles.navlink',
+        p: 2,
+      }}>
+      Contact
+    </Link>
+  </div>
+</header>
 );
